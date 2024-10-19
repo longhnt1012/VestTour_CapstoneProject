1439a7439f36c3f81943ebd792e0e23555216b05
longhnt <longhnt10122002@gmail.com>
2024-10-19 11:47:12 +0700

    Update

--- Backend/SEVestTourAPI/Models/CartItemModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SEVestTourAPI.Models
-{
-    public class CartItemModel
-    {
-        public int ProductId { get; set; }
-        public int Quantity { get; set; }
-    }
-}
--- Backend/SEVestTourAPI/Models/StyleModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SEVestTourAPI.Models
-{
-    public class StyleModel
-    {
-        public int StyleId { get; set; }          // Corresponds to StyleID in the database
-        public string? StyleName { get; set; }    // Name of the style
-        public string? Description { get; set; }   // Description of the style
-    }
-}
